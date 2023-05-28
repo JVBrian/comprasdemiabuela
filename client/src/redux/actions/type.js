90992770e490af3bc550300c16e68ae993e52be9
jvbrian <brianjarabav@hotmail.com>
2023-05-28 14:43:05 -0500

    Agregamos los componentes

--- client/src/redux/actions/type.js
+++ client/src/redux/actions/type.js
@@ -4,7 +4,9 @@ export const TOGGLE_TODO = "TOGGLE_TODO";
 export const UPDATE_TODO = "UPDATE_TODO";
 export const DELETE_TODO = "DELETE_TODO";
 export const TOGGLE_TAB = "TOGGLE_TAB";
-export const ALL_TODOS = "All Todos";
-export const ACTIVE_TODOS = "Active Todos";
-export const DONE_TODOS = "Done Todos";
+
+export const ALL_TODOS = "Todos los productos";
+export const ACTIVE_TODOS = "Productos";
+export const DONE_TODOS = "Productos comprados";
+
 export const TABS = [ALL_TODOS, ACTIVE_TODOS, DONE_TODOS];
